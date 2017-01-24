c6bc0cbe545a8952bb7bf2e78d7633cbd8f5beb5
Grigory Konushev <greg@narvar.com>
2017-01-24 15:35:17 -0800

    merged from master

--- src/main/kotlin/com/narvar/sqe/config/RedisConfiguration.kt
+++ src/main/kotlin/com/narvar/sqe/config/RedisConfiguration.kt
@@ -1,6 +1,5 @@
 package com.narvar.sqe.config
 
-import com.narvar.sqe.config.RedisCredentials.hostName
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
